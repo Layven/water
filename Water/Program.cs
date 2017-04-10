cf3a861530835761839ac0efe6b6d265083e949e
Layven <czovek.marton@gmail.com>
2017-04-10 03:57:48 +0200

    code added

---
+++ Water/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using SharpHelper;
+
+using Buffer11 = SharpDX.Direct3D11.Buffer;
+
+namespace Water {
+    class Program {
+
+        struct UniformData {
+            public Matrix worldViewProj;
+        }
+
+        static void Main(string[] args) {
+
+            if(!SharpDevice.IsDirectX11Supported()) {
+                System.Windows.Forms.MessageBox.Show("DirectX11 Not Supported");
+                return;
+            }
+
+            //render form
+            RenderForm form = new RenderForm();
+            form.Text = "Tutorial 4: Primitives";
+
+            //Help to count Frame Per Seconds
+            SharpFPS fpsCounter = new SharpFPS();
+
+            using(SharpDevice device = new SharpDevice(form)) {
+
+                //Init Mesh
+                Water water = new Water(device);
+
+                //Create Shader From File and Create Input Layout
+                SharpShader shader = new SharpShader(device, "../../HLSL.txt",
+                    new SharpShaderDescription() { VertexShaderFunction = "VS", PixelShaderFunction = "PS" },
+                    new InputElement[] {
+                        new InputElement("POSITION", 0, Format.R32G32B32_Float, 0, 0),
+                        new InputElement("COLOR", 0, Format.R32G32B32A32_Float, 12, 0)
+                    });
+
+                //create constant buffer
+                Buffer11 buffer = shader.CreateBuffer<UniformData>();
+
+                fpsCounter.Reset();
+
+                //main loop
+                RenderLoop.Run(form, () => {
+                    //Resizing
+                    if(device.MustResize) {
+                        device.Resize();
+                    }
+
+                    //apply states
+                    device.UpdateAllStates();
+
+                    //clear color
+                    device.Clear(Color.CornflowerBlue);
+
+                    //apply shader
+                    shader.Apply();
+
+                    //Set matrices
+                    float ratio = (float) form.ClientRectangle.Width / (float) form.ClientRectangle.Height;
+                    Matrix projection = Matrix.PerspectiveFovLH(3.14F / 3.0F, ratio, 1, 1000);
+                    Matrix view = Matrix.LookAtLH(new Vector3(0, 4, -10), new Vector3(), Vector3.UnitY);
+                    Matrix world = Matrix.RotationX(Environment.TickCount / 1000.0F);
+                    world = Matrix.Identity;
+                    Matrix WVP = world * view * projection;
+
+                    Vector3 phase = new Vector3(Environment.TickCount / 1000.0F, 0, 0);
+
+                    UniformData sceneInfo = new UniformData() {
+                        worldViewProj = WVP
+                    };
+
+                    //update constant buffer
+                    device.UpdateData<UniformData>(buffer, sceneInfo);
+
+                    //pass constant buffer to shader
+                    device.DeviceContext.VertexShader.SetConstantBuffer(0, buffer);
+
+                    //draw mesh
+                    water.Draw();
+
+                    //begin drawing text
+                    device.Font.Begin();
+
+                    //draw string
+                    fpsCounter.Update();
+                    device.Font.DrawString("FPS: " + fpsCounter.FPS, 0, 0);
+
+                    //flush text to view
+                    device.Font.End();
+                    //present
+                    device.Present();
+                });
+
+                //release resources
+                water.Dispose();
+                buffer.Dispose();
+            }
+
+
+
+        }
+    }
+}
---
+++ Water/Water.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpHelper;
+
+namespace Water {
+    public class Water : IDisposable {
+
+        //Indices
+        private int[] indices;
+
+        private int N = 50, M = 50;
+        private Vector3 botLeftCorner = new Vector3(-5.0f, -2.0f, -5.0f);
+        private float size = 10;
+
+
+        //Vertices
+        private ColoredVertex[] vertices;
+
+        private SharpMesh mesh;
+
+        public Water(SharpDevice device) {
+
+            vertices = new ColoredVertex[N * M];
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    vertices[i * N + j] = new ColoredVertex(
+                        new Vector3(i*size/N+ botLeftCorner.X, 0 + botLeftCorner.Y, j*size/M + botLeftCorner.Z),
+                        new Vector4((i + 0.0f) / M, (j + 0.0f) / N, 0, 1)
+                    );
+                }
+            }
+
+            foreach (ColoredVertex v in vertices) {
+                //Console.Out.WriteLine(v.Position);
+            }
+
+
+            indices = new int[6 * (N - 1) * (M - 1)];
+            for (int i = 0; i < M - 1; i++) {
+                for (int j = 0; j < N - 1; j++) {
+                    int act = i * (N - 1) + j;
+                    int fullIndex = i * N + j;
+
+                    indices[6 * act + 0] = fullIndex;
+                    indices[6 * act + 1] = fullIndex + 1;
+                    indices[6 * act + 2] = fullIndex + N;
+                    indices[6 * act + 3] = fullIndex + 1;
+                    indices[6 * act + 4] = fullIndex + N + 1;
+                    indices[6 * act + 5] = fullIndex + N;
+                }
+            }
+
+            this.createMesh(device);
+
+        }
+
+        public SharpMesh createMesh(SharpDevice device) {
+            this.mesh = SharpMesh.Create<ColoredVertex>(device, vertices, indices);
+            return mesh;
+        }
+        
+
+        public void Draw() {
+            mesh?.Draw();
+        }
+
+        public void Dispose() {
+            mesh?.Dispose();
+        }
+    }
+
+
+
+}
